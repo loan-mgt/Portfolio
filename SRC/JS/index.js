08e91fab7dc05b0c0b8a24f112486c6899d09915
Qypol342 <Qypol342@gmail.com>
2022-12-05 22:52:55 +0100

    add calculate commit count

---
+++ SRC/JS/index.js
@@ -0,0 +1,56 @@
+console.log("loaded")
+const commitCounter = document.querySelector("#commit");
+let commit = []
+
+
+async function  getAllRepo(name) {
+	let res = await fetch("https://api.github.com/users/"+name+"/repos")
+	res = await  res.json()
+	let repos = [];
+	if (res.length>0){
+		res.forEach((repo) =>{
+			repos.push(repo["name"])
+		});
+	}
+	return repos;
+	
+}
+
+async function getCommit(name, repo) {
+	let res = await fetch("https://api.github.com/repos/"+name+"/"+repo+"/commits")
+	res =await res.json()
+	return res.length
+}
+
+async function updateCommit() {
+	const  name = "qypol342";
+	const repos = await getAllRepo(name)
+	let commit = []
+	for (var i = 0; i < repos.length; i++) {
+		
+		const tmp_commit = await getCommit(name,repos[i]);
+		commit.push( tmp_commit);
+	}
+
+	console.log(commit)
+}
+
+async function updateCommitEl(el) {
+	const  name = "qypol342";
+	const repos = await getAllRepo(name)
+	
+	for (var i = 0; i < repos.length; i++) {
+		
+		getCommit(name,repos[i])
+		.then(function(tmp_commit) {
+			commit.push( tmp_commit)
+			el.innerHTML = commit.length});
+
+	}
+
+	console.log(commit)
+}
+
+
+updateCommitEl(commitCounter)
+.then(function() {console.log("end")})
