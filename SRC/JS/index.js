7c1c7d7b599312aec0160c71096ab64cc5e742c0
Qypol342 <Qypol342@gmail.com>
2023-03-22 18:11:56 +0100

    scroll on bage click

--- SRC/JS/index.js
+++ SRC/JS/index.js
@@ -4,6 +4,16 @@ const tableDiv = document.querySelector(".back-table");
 const loadingSplash = document.querySelector(".loading");
 const skills = document.querySelectorAll(".sub-panel");
 const skillBtns = document.querySelectorAll(".skill-title");
+const bages = document.querySelectorAll(".d-badge");
+const skillsHash = {
+	"Développer": document.querySelector("#dev"),
+	"Optimiser": document.querySelector("#opti"),
+	"Administrer": document.querySelector("#admin"),
+	"Gérer": document.querySelector("#manag"),
+	"Conduire": document.querySelector("#drive"),
+	"Collaborer": document.querySelector("#coll"),
+
+}
 let tableShadowPos = 0;
 
 loadingSplash.style.zIndex = -2;
@@ -27,6 +37,21 @@ addEventListener("scroll", (event) => {
 
 });
 
+for(const bage of bages){
+	bage.addEventListener("click", (event) =>{
+		console.log(event.target.innerText )
+		window.location.hash = "";
+		window.location.hash = "skill";
+		const target = skillsHash[event.target.innerText ];
+		console.log(target)
+		
+		showSkill(target.id);
+		showActif(target);
+
+		});
+		
+
+}
 
 
 let count = 0
@@ -69,74 +94,3 @@ function showSkill(id){
 
 
 
-async function  getAllRepo(name) {
-	let res = await fetch("https://api.github.com/users/"+name+"/repos")
-	res = await  res.json()
-	let repos = [];
-	if (res.length>0){
-		res.forEach((repo) =>{
-			repos.push(repo["name"])
-		});
-	}
-	return repos;
-	
-}
-
-async function getCommit(user, owner, repo) {
-  let page = 0
-  let count = 0;
-  let status =200;
-  res_len = -1;
-  while (res_len != 0 && status===200 ){
-  	console.log("status", status)
-  	try{
-  	let res = await fetch(`https://api.github.com/repos/${owner}/${repo}/commits?author=${user}&per_page=100&page=${page}`);
-  	res = await res.json();
-  	count += res.length;
-  	res_len = res.length;
-
-  	status = await res.status
-  	} catch{
-  		res_len = 0
-  	}finally{
-  		page++;
-  	}
-  }
-  
-  return count === undefined ? 0 : count  ;
-}
-
-async function updateCommit() {
-	const  name = "qypol342";
-	const repos = await getAllRepo(name)
-	let commit = []
-	for (var i = 0; i < repos.length; i++) {
-		
-		const tmp_commit = await getCommit(name,name,repos[i]);
-		commit.push( tmp_commit);
-	}
-
-	console.log(commit)
-}
-
-async function updateCommitEl(el) {
-	const  name = "qypol342";
-	const repos = await getAllRepo(name)
-	let commit = 0
-	
-	for (var i = 0; i < repos.length; i++) {
-		commit += await getCommit(name,name,repos[i]) 
-	}
-	console.log("commit brefor tchou", commit)
-	commit +=  await getCommit(name,'icepick4','TchouTchou'); 
-	console.log("commit after tchou",commit)
-	el.innerHTML = "+" +commit ;
-
-
-
-	console.log(" final number",commit)
-}
-
-
-// updateCommitEl(commitCounter)
-// .then(function() {console.log("end")})
