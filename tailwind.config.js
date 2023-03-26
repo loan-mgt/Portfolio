a01f20511aadaeddf2b404564c1f0363aa5c3439
Qypol342 <Qypol342@gmail.com>
2023-03-26 16:17:55 +0200

    clean up

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: ["./src/**/*.{html,js}","./*.{html,js}"],
   theme: {
+    colors :{
+      'pblue' : 'rgb(12, 72, 250);',
+    },
     extend: {},
   },
   plugins: [require("daisyui")],
